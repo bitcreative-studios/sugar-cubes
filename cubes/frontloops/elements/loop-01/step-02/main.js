bd878d6005934419f7352c1761349f38f4a518fa
George Bockari <bitwhys@gmail.com>
2019-10-21 02:03:02 -0700

    feat: finished static styles for elmnt-L01-S02

--- cubes/frontloops/elements/loop-01/step-02/main.js
+++ cubes/frontloops/elements/loop-01/step-02/main.js
@@ -1,1 +1,20 @@
 import "./scss/styles.scss"
+
+/**
+ * Organize content in common `JSON` structure
+ * In a real application this would be our payload
+ * from an API call
+ */
+const data = {
+  positive: "Positive content",
+  neutral: "Neutral content",
+  negative: "Negative content",
+}
+
+const modifiers = {
+  activeClass: "btn--active",
+}
+
+const attributes = {
+  value: "data-value",
+}
