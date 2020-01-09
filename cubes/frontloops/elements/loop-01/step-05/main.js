6f87a387e0a2d32000afb92a4e6297f7f6ca7e51
George Bockari <bitwhys@gmail.com>
2020-01-09 11:12:37 -0800

    refactor: creates a span for each character of a service, fixes the position of the text

--- cubes/frontloops/elements/loop-01/step-05/main.js
+++ cubes/frontloops/elements/loop-01/step-05/main.js
@@ -1,14 +1,31 @@
 import "./scss/styles.scss"
 
 const services = ["design", "frontend", "backend", "testing"]
-
 const textPrompt = document.querySelector(".text-prompt")
+const classes = ["character"]
+let current = 0
 
-let index = 0
-setInterval(function() {
-  if (index === services.length) {
-    index = 0
-  }
-  textPrompt.innerText = services[index]
-  index++
+/**
+ * 1. Split the word into an array of characters
+ * 2. For each character create a span element with the character as the content
+ * 3. Add these spans to the DOM
+ *
+ */
+
+const spanCharacter = character => {
+  const span = document.createElement("span")
+  span.classList.add(...classes)
+  span.innerText = character
+  return span
+}
+
+setInterval(() => {
+  if (current === services.length) current = 0
+  // clear current content in text-prompt
+  textPrompt.innerHTML = ""
+  const characters = services[current].split("")
+  characters.forEach(character => {
+    textPrompt.appendChild(spanCharacter(character))
+  })
+  ++current
 }, 1000)
