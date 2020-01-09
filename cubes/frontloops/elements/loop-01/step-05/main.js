fbe8d206437e587a5586d60abfc58efaa6c3c59b
George Bockari <bitwhys@gmail.com>
2020-01-09 03:52:24 -0800

    feat: naive implementation of design

--- cubes/frontloops/elements/loop-01/step-05/main.js
+++ cubes/frontloops/elements/loop-01/step-05/main.js
@@ -1,1 +1,14 @@
 import "./scss/styles.scss"
+
+const services = ["design", "frontend", "backend", "testing"]
+
+const textPrompt = document.querySelector(".text-prompt")
+
+let index = 0
+setInterval(function() {
+  if (index === services.length) {
+    index = 0
+  }
+  textPrompt.innerText = services[index]
+  index++
+}, 1000)
