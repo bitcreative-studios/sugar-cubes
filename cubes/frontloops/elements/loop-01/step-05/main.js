9c1f12a70312b2cef446a42896474f4e3cec78b8
George Bockari <bitwhys@gmail.com>
2020-01-09 16:23:38 -0800

    feat: uses Traversy Media tutorial to implement design

--- cubes/frontloops/elements/loop-01/step-05/main.js
+++ cubes/frontloops/elements/loop-01/step-05/main.js
@@ -2,30 +2,59 @@ import "./scss/styles.scss"
 
 const services = ["design", "frontend", "backend", "testing"]
 const textPrompt = document.querySelector(".text-prompt")
-const classes = ["character"]
-let current = 0
 
-/**
- * 1. Split the word into an array of characters
- * 2. For each character create a span element with the character as the content
- * 3. Add these spans to the DOM
- *
- */
-
-const spanCharacter = character => {
-  const span = document.createElement("span")
-  span.classList.add(...classes)
-  span.innerText = character
-  return span
+const TypeWriter = function TypeWriter(textElement, words, delay = "3000") {
+  console.log(this)
+  this.textElement = textElement
+  this.words = words
+  this.txt = ""
+  this.currentWord = 0
+  this.delay = parseInt(delay, 10)
+  this.type()
+  this.isDeleting = false
 }
 
-setInterval(() => {
-  if (current === services.length) current = 0
-  // clear current content in text-prompt
-  textPrompt.innerHTML = ""
-  const characters = services[current].split("")
-  characters.forEach(character => {
-    textPrompt.appendChild(spanCharacter(character))
-  })
-  ++current
-}, 1000)
+function init() {
+  const textPrompt = document.querySelector(".text-prompt")
+  let { words, delay } = textPrompt.dataset
+  words = JSON.parse(words)
+  // FIXME: using new to create a context for `this` should rewrite as functions
+  new TypeWriter(textPrompt, words, delay)
+}
+
+// Type Method
+TypeWriter.prototype.type = function type() {
+  // Current word
+  const currentWord = this.currentWord % this.words.length
+  // Get full word text
+  const fullText = this.words[currentWord]
+  // check if deleting
+  if (this.isDeleting) {
+    //  Remove character
+    this.txt = fullText.substring(0, this.txt.length - 1)
+  } else {
+    this.txt = fullText.substring(0, this.txt.length + 1)
+  }
+  // Push this.txt into the DOM
+  this.textElement.innerHTML = `<span class="txt cursor">${this.txt}</span>`
+
+  // Initial type speed (when deleting we want the speed to increase)
+  let typeSpeed = 300
+  if (this.isDeleting) typeSpeed /= 2
+
+  // Check if finished with word
+  if (!this.isDeleting && this.txt === fullText) {
+    // Make a pause at end
+    typeSpeed = this.delay
+    // set delete to true
+    this.isDeleting = true
+  } else if (this.isDeleting && !this.txt) {
+    this.isDeleting = false
+    this.currentWord++
+    typeSpeed = 500
+  }
+  setTimeout(() => this.type(), typeSpeed)
+}
+
+// Init on DOM load
+document.addEventListener("DOMContentLoaded", init)
