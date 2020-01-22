97a4dd95b172efa9321b944b504290c7ca397b05
George Bockari <bitwhys@gmail.com>
2020-01-22 05:21:50 -0800

    feat: adds solution for challenge. LUL IntersectionObserver, easy as pie

--- cubes/frontloops/elements/loop-01/step-07/main.js
+++ cubes/frontloops/elements/loop-01/step-07/main.js
@@ -1,1 +1,16 @@
 import "./scss/styles.scss"
+
+const sections = [...document.querySelectorAll(".content__area.hidden")]
+const observer = new IntersectionObserver(
+  changes => {
+    const [changed] = changes
+    console.log(changes)
+    if (changed.isIntersecting) changed.target.classList.remove("hidden")
+  },
+  {
+    rootMargin: "10px",
+    threshold: 0.3,
+  }
+)
+
+sections.forEach(section => observer.observe(section))
