608ed106149d35a8240cbf9ea118e175d06871ba
George Bockari <bitwhys@gmail.com>
2020-01-22 00:57:43 -0800

    feat: adds back button functionality. completes challenge

--- cubes/frontloops/elements/loop-01/step-04/main.js
+++ cubes/frontloops/elements/loop-01/step-04/main.js
@@ -16,6 +16,8 @@ const completed = []
 // needing to create it here
 const backButton = document.createElement("button")
 backButton.classList.add("btn", "text")
+let ACTIVE_STEP
+let FORM_COMPLETED = false
 
 
 // collection of available steps
@@ -74,7 +76,8 @@ function updateUI(name) {
   toolTip.innerText = toolTipText
 }
 
-function handleStepChange(name) {
+function handleStepChange(name, goBack = false) {
+  if(FORM_COMPLETED) return
   //  determine if UI should be updated
   //  get step number
 
@@ -88,36 +91,50 @@ function handleStepChange(name) {
     completed.push(previousStep)
     steps[previousStep].classList.remove("active")
     steps[name].classList.add("active")
+    ACTIVE_STEP = name
     updateUI(name)
   } else if (completed.includes(name)) {
     // case: moving backward
     const numberOfPops = completed.length - 1 - completed.indexOf(name)
-    document.querySelector(".active").classList.remove("active")
+    steps[ACTIVE_STEP].classList.remove("active")
+    // document.querySelector(".active").classList.remove("active")
     for (let i = 0; i < numberOfPops + 1; i++) {
       // eslint-disable-next-line no-shadow
       const name = completed.pop()
       steps[name].classList.remove("completed")
     }
     steps[name].classList.add("active")
+    ACTIVE_STEP = name
     updateUI(name)
+  } else if (goBack) {
+    if(name === "title") return
+    steps[name].classList.remove("active")
+    ACTIVE_STEP = completed[completed.length -1]
+    handleStepChange(ACTIVE_STEP)
   }
 }
 function handleSubmit() {
   // get active step
   const activeStep = document.querySelector(".active").dataset.step
   if (activeStep === "confirm") {
+    steps[ACTIVE_STEP].classList.add("completed")
+    FORM_COMPLETED = true
     updateUI("success")
   } else {
     let [, currentStep] = document.querySelector(".active").id.split("-")
     currentStep = Number(currentStep)
     const nextStep = idToName[`step-${currentStep + 1}`]
-    console.log(nextStep)
     handleStepChange(nextStep)
   }
 }
+function handleBackClick() {
+  const active = document.querySelector(".active")
+  const {step} = active.dataset
+  handleStepChange(step,true)
+}
 
-function handleBackClick() {}
 submitButton.addEventListener("click", handleSubmit)
+backButton.addEventListener("click",handleBackClick)
 const checkboxes = Array.from(document.querySelectorAll("[type='checkbox']"))
 checkboxes.forEach(checkbox =>
   checkbox.addEventListener("change", e => handleStepChange(e.target.name))
