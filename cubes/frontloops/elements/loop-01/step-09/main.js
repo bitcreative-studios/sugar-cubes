4a73cceba1aa3f20c62e66f0c0b6a9fc37d8c7e5
George Bockari <bitwhys@gmail.com>
2020-01-23 21:51:54 -0800

    feat: adds static design

--- cubes/frontloops/elements/loop-01/step-09/main.js
+++ cubes/frontloops/elements/loop-01/step-09/main.js
@@ -1,1 +1,11 @@
 import "./scss/styles.scss"
+
+const stickyContent = document.querySelector(".sticky-content")
+const bitwhys = "author"
+
+window.addEventListener("scroll", () => {
+  console.log("WINDOW IS SCROLLING")
+  const { position } = getComputedStyle(stickyContent, "position")
+  console.log(position)
+})
+console.log(bitwhys)
